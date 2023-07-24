60a1911d8e25fe2125cb0f11bfd728354fa24b7d
Hyun, Jaeyeon <jayhyun1008@gmail.com>
2023-07-25 08:28:37 +0900

    Create MkCustomChart.vue

---
+++ packages/frontend/src/components/MkCustomChart.vue
@@ -0,0 +1,40 @@
+<template>
+<div :class="$style.root">
+	<canvas :id="chartId"></canvas>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref, shallowRef, watch, PropType } from 'vue';
+import { Chart } from 'chart.js';
+import gradient from 'chartjs-plugin-gradient';
+
+const props = defineProps<{
+	chartId: string;
+}>(); 
+
+const ctx = document.getElementById(props.chartId);
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+</script>
+
+<style lang="scss" module>
+</style>
