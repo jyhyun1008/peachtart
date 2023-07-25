b59d0857a1933c27a2a6a592cb48d217ad2eca03
Hyun, Jaeyeon <jayhyun1008@gmail.com>
2023-07-26 06:23:16 +0900

    className

--- packages/frontend/src/components/MkCustomChart.vue
+++ packages/frontend/src/components/MkCustomChart.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-	<canvas :id="chartId" :class="[{ title: 'title' }, { keys: 'keys' }, { values: 'values' }, { label: 'label' }]"></canvas>
+<div ref="rootEl">
+	<canvas :id="chartId" :class="[{ title: 'title' }, { keys: 'keys' }, { values: 'values' }, { label: 'label' }, { className: 'className' }]"></canvas>
 </div>
 </template>
 
@@ -17,10 +17,12 @@ const props = defineProps<{
 	keys: string[];
 	values: number[];
 	label: string;
+	className: string;
 }>(); 
 
 onMounted(() => {
 	const ctx = document.getElementById(props.chartId);
+	this.$refs.canvasEl.classList.add(props.className);
 
   new Chart(ctx, {
     type: 'bar',
