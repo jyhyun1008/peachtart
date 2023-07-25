5dd0bd901c40cc1f5cd1d798bce68d4f1d0f2400
Hyun, Jaeyeon <jayhyun1008@gmail.com>
2023-07-25 09:57:42 +0900

    Update MkCustomChart.vue: 프로퍼티 수정해서 유저가 값을 입력할 수 있게

--- packages/frontend/src/components/MkCustomChart.vue
+++ packages/frontend/src/components/MkCustomChart.vue
@@ -11,9 +11,29 @@ import gradient from 'chartjs-plugin-gradient';
 
 Chart.register(...registerables);
 	
-const props = defineProps<{
-	chartId: string;
-}>(); 
+const props = defineProps({
+	chartId: {
+		type: String,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: false,
+		default: 'My Chart',
+	},
+	keys: {
+		type: String[],
+		required: true,
+	},
+	values: {
+		type: number[],
+		required: true,
+	},
+	label: {
+		type: String,
+		required: false,
+	}
+}); 
 
 onMounted(() => {
 	const ctx = document.getElementById(props.chartId);
@@ -21,14 +41,25 @@ onMounted(() => {
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: props.keys,
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
+        label: props.label,
+        data: props.values,
+        borderColor: Utils.CHART_COLORS.red,
+        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+        borderWidth: 1,
+        borderRadius: 5,
+        borderSkipped: false,
       }]
     },
     options: {
+			responsive: true,
+			plugins: {
+	      title: {
+	        display: true,
+	        text: props.title
+	      }
+			},
       scales: {
         y: {
           beginAtZero: true
@@ -38,7 +69,8 @@ onMounted(() => {
   });
 });
 
-
+Chart.defaults.borderColor = '#36A2EB';
+Chart.defaults.color = '#000';
 
 </script>
 
