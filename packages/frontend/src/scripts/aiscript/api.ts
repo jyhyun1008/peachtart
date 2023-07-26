43ed8afbee69469f2996eaded0b0c3d5a13e205e
jyhyun1008 <hyun1008@kasi.re.kr>
2023-07-26 10:33:03 +0900

    revert

--- packages/frontend/src/scripts/aiscript/api.ts
+++ packages/frontend/src/scripts/aiscript/api.ts
@@ -45,10 +45,6 @@ export function createAiScriptEnv(opts) {
 			utils.assertNumber(tofixed);
 			return values.NUM(num.value.toFixed(tofixed.value));
 		}),
-		'Str:parseFloat': values.FN_NATIVE(([str]) => {
-			utils.assertString(str);
-			return values.NUM(parseFloat(str.value));
-		}),
 		'Date:weekDay': values.FN_NATIVE(([date]) => {
 			utils.assertNumber(date);
 			var result = new Date(date.value).getDay();
