a72560276f060d26370b07f1009560ddf10f1f51
jyhyun1008 <hyun1008@kasi.re.kr>
2023-07-30 13:59:28 +0900

    URIreplace

--- packages/frontend/src/scripts/aiscript/api.ts
+++ packages/frontend/src/scripts/aiscript/api.ts
@@ -66,6 +66,16 @@ export function createAiScriptEnv(opts) {
 			regEx = new RegExp(regEx.value, 'gm');
 			return values.STR(str.value.replace(regEx, replace.value))
 		}),
+		'Str:encodeURI': values.FN_NATIVE(([url]) => {
+			utils.assertString(url);
+			return values.STR(encodeURI(url.value))
+		}),
+		'Str:URIReplace': values.FN_NATIVE(([str]) => {
+			utils.assertString(str);
+			var regEx0 = new RegExp('\(https\:\/\/([^\)\s]+)\)\s', 'gm');
+			var regEx1 = new RegExp('https\:\/\/([^\s]+)\s', 'gm');
+			return values.STR(str.value.replace(regEx0, encodeURI).replace(regEx1, encodeURI))
+		}),
 		'Mk:fetchMd': values.FN_NATIVE(async([githubUserName, repoName, branchName, fileName]) => {
 			utils.assertString(githubUserName);
 			utils.assertString(repoName);
@@ -75,10 +85,6 @@ export function createAiScriptEnv(opts) {
 			var response = await fetch(url)
 			var markdown = await response.text();
 			return values.STR(markdown)
-		}),
-		'Str:encodeURI': values.FN_NATIVE(([url]) => {
-			utils.assertString(url);
-			return values.STR(encodeURI(url.value))
 		}),
 		'Mk:keyDown': values.FN_NATIVE(([key, fn], opts) => {
 			utils.assertNumber(key);
