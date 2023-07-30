b0adad0b28c56d541c76125c2ba8bbbcc3ddf2da
jyhyun1008 <hyun1008@kasi.re.kr>
2023-07-30 14:41:13 +0900

    ?...어쨌든 안되길래 수정함... \s를 제껴버림

--- packages/frontend/src/scripts/aiscript/api.ts
+++ packages/frontend/src/scripts/aiscript/api.ts
@@ -68,7 +68,7 @@ export function createAiScriptEnv(opts) {
 		}),
 		'Str:URIReplace': values.FN_NATIVE(([str]) => {
 			utils.assertString(str);
-			var regEx0 = new RegExp('\(https\:\/\/([^\)\s]+)\)', 'gm');
+			var regEx0 = new RegExp('\(https\:\/\/([^\)]+)\)', 'gm');
 			var regEx1 = new RegExp('https\:\/\/([^\)\s]+)\s', 'gm');
 			return values.STR(str.value.replace(regEx0, encodeURI).replace(regEx1, encodeURI))
 		}),
