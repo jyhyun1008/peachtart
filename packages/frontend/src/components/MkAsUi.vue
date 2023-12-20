93a2f96c8a9a58b584b94d042952484f4aa6a7ba
Hyun, Jaeyeon <jayhyun1008@gmail.com>
2023-12-21 08:50:03 +0900

    Update MkAsUi.vue

--- packages/frontend/src/components/MkAsUi.vue
+++ packages/frontend/src/components/MkAsUi.vue
@@ -10,6 +10,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<MkAsUi v-if="!g(child).hidden" :component="g(child)" :components="props.components" :size="size"/>
 		</template>
 	</div>
+	<div v-else-if="c.type === 'HTML'" :class="[ c.className ]"><style>{{ c.css }}</style>{{ c.HTML }}</div>
 	<span v-else-if="c.type === 'text'" :class="[{ [$style.fontSerif]: c.font === 'serif', [$style.fontMonospace]: c.font === 'monospace' }, c.className]" :style="{ fontSize: c.size ? `${c.size * 100}%` : null, fontWeight: c.bold ? 'bold' : null, color: c.color ?? null }">{{ c.text }}</span>
 	<Mfm v-else-if="c.type === 'mfm'" :class="[{ [$style.fontSerif]: c.font === 'serif', [$style.fontMonospace]: c.font === 'monospace' }, c.className]" :style="{ fontSize: c.size ? `${c.size * 100}%` : null, fontWeight: c.bold ? 'bold' : null, color: c.color ?? null }" :text="c.text"/>
 	<MkButton v-else-if="c.type === 'button'" :primary="c.primary" :className="c.className" :rounded="c.rounded" :disabled="c.disabled" :small="size === 'small'" inline @click="c.onClick">{{ c.text }}</MkButton>
