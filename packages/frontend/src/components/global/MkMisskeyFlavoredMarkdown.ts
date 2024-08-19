76e788028dd66fbac7132b8ec4c23d8d5f9ac2fb
HiYUNo <jayhyun1008@gmail.com>
2024-08-19 11:20:24 +0900

    Update MkMisskeyFlavoredMarkdown.ts

--- packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
+++ packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
@@ -114,7 +114,8 @@ export default function (props: MfmProps, { emit }: { emit: SetupContext<MfmEven
 					text = text.replace(/[\-]{3}/g, '<hr>');
 
 					//br
-					text = text.replace(/\>\n/gm, '')
+					text = text.replace(/\>\n\n/gm, '')
+					text = text.replace(/\>\n\</gm, '')
 
 					return text
 				}
