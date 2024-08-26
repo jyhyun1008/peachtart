a40abad86da71ccb5a26aa88efc7b1e089ffd815
HiYUNo <jayhyun1008@gmail.com>
2024-08-26 09:13:46 +0900

    ad if type==br

--- packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
+++ packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
@@ -574,7 +574,9 @@ export default function (props: MfmProps, { emit }: { emit: SetupContext<MfmEven
 
 	if (result.children) {
 		for (var i=0; i<result.children.length; i++) {
-			if (result.children[i].props) {
+			if (result.children[i].type == 'br'){
+				resultplain += '<br><!-- -->'
+			} else if (result.children[i].props) {
 				if (result.children[i].props.innerHTML) {
 					resultplain += result.children[i].props.innerHTML + '<!-- -->'
 				} else if (result.children[i].props.emoji) {
