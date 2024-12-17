d41119d26c5b8e3271e36be93a004601ea4493c0
jyhyun1008 <jayhyun1008@gmail.com>
2024-12-18 06:25:16 +0900

    Update MkMfm.ts

--- packages/frontend/src/components/global/MkMfm.ts
+++ packages/frontend/src/components/global/MkMfm.ts
@@ -92,8 +92,8 @@ export default function (props: MfmProps, { emit }: { emit: SetupContext<MfmEven
 	//props.text = props.text.replace(/\|/gm, '</i><i>td1 ')
 	props.text = props.text.replace(/\<i\>td1\s<\/table>\n<table>\<\/i\>\<i\>td1\s(.+)\-{2,}(.+)\<\/i\>\<i\>td1\s<\/table>\n<table>\<\/i\>/g, '</i></i><i>tbody <i>tr ')
 	props.text = props.text.replace(/\<i\>td1\s<\/table>\n<table>\<\/i\>/g, '</i><i>tr ')
-	props.text = props.text.replace(/^\<\/i\>/gm, '<i>table <i>thead <i>tr ')
-	props.text = props.text.replace(/\<i\>td1\s$/g, '</i></i></i>')
+	props.text = props.text.replace(/^<table>\<\/i\>/gm, '<i>table <i>thead <i>tr ')
+	props.text = props.text.replace(/\<i\>td1\s<\/table>$/g, '</i></i></i>')
 	props.text = props.text.replace(/\<i\>td1\s<\/table>\n<table>/g, '</i></i></i>')
 
 	// hr
